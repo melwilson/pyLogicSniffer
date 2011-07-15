5ba3f83bafa70c86554464b3e1b483a1120ef29f
Mel Wilson <mwilson@tecumseth.(none)>
2011-07-15 15:42:12 -0400

    Add sump_metadata.py, which should have been included in previous commit.

---
+++ sump_metadata.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+'''Configuration dialog for a SUMP logic-analyzer device.
+Copyright © 2011, Mel Wilson mwilson@melwilsonsoftware.ca
+
+This file is part of pyLogicSniffer.
+
+    pyLogicSniffer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyLogicSniffer is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyLogicSniffer.  If not, see <http://www.gnu.org/licenses/>.
+'''
+headings_en = {
+	0x01: 'Device name',
+	0x02: 'Firmware version',
+	0x03: 'PIC version',
+	0x20: 'Probes',
+	0x21: 'Sample memory size',
+	0x22: 'Dynamic memory size',
+	0x23: 'Maximum rate',
+	0x24: 'Protocol version',
+	0x40: 'Probes',
+	0x41: 'Protocol version',
+}
+
+headings = headings_en
